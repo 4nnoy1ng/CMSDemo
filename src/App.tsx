4310fc37a688850a01b4bafa711cd001d4f090d1
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-12-17 11:35:49 +0100

    vercel error should be fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Card from './components/Card';
 function App() {
   return (
     <>
-      <div className="pt-20 h-32 text-white font-bold text-5xl pr-8 pt-8 bg-yellow-900   flex justify-end" >HTL Dornbirn 5bWI</div>
+      <div className="pt-20 h-32 text-white font-bold text-5xl pr-8 bg-yellow-900   flex justify-end" >HTL Dornbirn 5bWI</div>
       <div className=" bg-yellow-900 m-20 pb-48 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 ">
         <Card />
         <Card />
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 
 interface Props {
 
@@ -6,8 +6,8 @@ interface Props {
 
 export default function Card({ }: Props): ReactElement {
     return (
-        <div className=" m-10 bg-yellow-900 w-full object-fit-contain border-2 bg-white  rounded-lg"> 
-            <img className="w-full h-48 object-cover rounded-t-lg" src="https://picsum.photos/300/300" alt="" />
+        <div className=" m-10 bg-white w-full object-fit-contain border-2  rounded-lg"> 
+            <img className="w-full h-48 object-cover rounded-t-lg" src="https://picsum.photos/600/600" alt="" />
             <div className=" ml-5 mr-5 mb-5 mt-2 bg bg-white font-bold">Name</div>
             <div className="m-5">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  </div>
         </div>
